845bfa4c78a8203d44bd4cf785ad604eac4db187
carlossantos.desenvolvedor@gmail.com <carlossantos.desenvolvedor@gmail.com>
2020-05-15 12:53:52 -0300

    Criado Exercicio verificar se o número é crescente ou decrescente

---
+++ ExerciciosLogica/ExerciciosLogica/Logica/NumeroCrescenteDecrescente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosLogica.Logica
+{
+    class NumeroCrescenteDecrescente
+    {
+        public static void Executar()
+        {
+            int x, y;
+            string[] vet = Console.ReadLine().Split(" ");
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                vet = Console.ReadLine().Split(" ");
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
--- ExerciciosLogica/ExerciciosLogica/Program.cs
+++ ExerciciosLogica/ExerciciosLogica/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ExerciciosLogica
+using ExercicioLogica;
+using ExerciciosLogica.Logica;
+
+namespace ExercicioLogica
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Verificar se o número é crescente ou decrescente", NumeroCrescenteDecrescente.Executar},
+            });
+
+            central.SelecionarEExecutar();
         }
     }
 }
